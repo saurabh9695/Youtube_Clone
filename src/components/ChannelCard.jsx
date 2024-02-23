c9d29002896afc9e8a9ca0fc87619ef7b3290473
Saurabh <saurabhkushwaha2030@gmail.com>
2024-02-24 03:46:16 +0530

    update some file

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, CardContent, Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Link } from 'react-router-dom';
-import { demoProfilePicture } from '../utils/constants';
+
 
 const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
@@ -20,11 +20,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
-        <CardMedia
-          image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
-          alt={channelDetail?.snippet?.title}
-          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
-        />
+      
         <Typography variant="h6">
           {channelDetail?.snippet?.title}{' '}
           <CheckCircleIcon sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
